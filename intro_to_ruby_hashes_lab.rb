46dfdbeddc2c12da3b1205b2135dbad8d5e73a87
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-18 22:08:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,8 @@ end
 def monopoly_with_third_tier
   monopoly = {
   railroads: {pieces: 4,
-  rent_in_dollars: {one:1, two:2, three:3, four:4,
-  
-     }
+  rent_in_dollars: {one:1, two:2, three:3, four:4},
+  names: {}
    }
 }
 end
