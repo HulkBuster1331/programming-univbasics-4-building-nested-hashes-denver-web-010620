0bdc78c90e2c11526ff162ab5bb2955f57fad100
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-18 22:23:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
                      two_pieces_owned:50, 
                        three_pieces_owned:100, 
                          four_pieces_owned:200},
-  names: {reading_railroad:{}, 
+  names: {reading_railroad:{mortgage: $100}, 
            pennsylvania_railroad:{}, 
              b_and_o_railroad: {},
                 shortline_railroad: {}}
